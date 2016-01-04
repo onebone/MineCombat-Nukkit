dcc89dc1a2bc4ed86e9a49755fba2fc99833d15e
onebone <jyc00410@gmail.com>
2016-01-04 23:33:33 +0900

    Added spawn position settings
    Implemented match system
    
    Add LICENSE

--- src/main/java/me/onebone/minecombat/MineCombat.java
+++ src/main/java/me/onebone/minecombat/MineCombat.java
@@ -1,10 +1,31 @@
 package me.onebone.minecombat;
 
+/*
+ * MineCombat: FP..S? for Nukkit
+ * Copyright (C) 2016 onebone <jyc00410@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import me.onebone.minecombat.data.PlayerContainer;
 import me.onebone.minecombat.gun.Pistol;
+import me.onebone.minecombat.task.StartGameTask;
+import me.onebone.minecombat.task.StopGameTask;
 import me.onebone.minecombat.task.TickTask;
 import cn.nukkit.Player;
 import cn.nukkit.command.Command;
@@ -23,6 +44,9 @@ public class MineCombat extends PluginBase implements Listener{
 	public static final int STATUS_STOPPED = 0;
 	public static final int STATUS_ONGOING = 1;
 	
+	public static final int TEAM_RED = 0;
+	public static final int TEAM_BLUE = 1;
+	
 	public static final int GUN_ITEM_ID = 104;
 	
 	public static final String STATUS_FORMAT = 
@@ -30,26 +54,39 @@ public class MineCombat extends PluginBase implements Listener{
 			+ "Ammo: %ammo/%magazine";
 	
 	private Map<String, PlayerContainer> containers = null;
+	private Map<String, Map<String, Object[]>> position = null;
 	
-	private int status = STATUS_ONGOING; // TODO
+	private int status = STATUS_STOPPED;
 	
 	public void onEnable(){
-		containers = new HashMap<>();
-		
-		this.getServer().getPluginManager().registerEvents(this, this);
-		
-		this.getServer().getScheduler().scheduleDelayedRepeatingTask(new TickTask(this), 10, 10);
+		this.saveDefaultConfig();
+		Map<String, Map<String, Object[]>> spawns = this.getConfig().get("spawn-pos", new LinkedHashMap<String, Map<String, Object[]>>());
+		position = new HashMap<>();
+		if(spawns == null || spawns.size() == 0){
+			this.getLogger().warning("No spawn position was given. Set spawns using /spawnpos");
+		}else{
+			containers = new HashMap<>();
+			
+			this.getServer().getPluginManager().registerEvents(this, this);
+			
+			this.getServer().getScheduler().scheduleDelayedRepeatingTask(new TickTask(this), 10, 10);
+			this.getServer().getScheduler().scheduleDelayedTask(new StartGameTask(this), this.getConfig().get("prepare-game", 60) * 20);
+		}
 	}
-	
+
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event){
 		Player player = event.getPlayer();
 		
-		if(!containers.containsKey(player.getName())){
-			containers.put(player.getName(), new PlayerContainer(player, new Pistol(this, player)));
+		if(this.status == STATUS_ONGOING){
+			if(!containers.containsKey(player.getName())){
+				int redTeam = this.countPlayers(TEAM_RED);
+				int team = containers.size() - redTeam < redTeam ? TEAM_BLUE : TEAM_RED; 
+				containers.put(player.getName(), new PlayerContainer(player, new Pistol(this, player), team));
+			}
+			
+			containers.get(player.getName()).setActive();
 		}
-		
-		containers.get(player.getName()).setActive();
 		
 		player.getInventory().setItem(0, Item.get(GUN_ITEM_ID));
 		player.getInventory().setHotbarSlotIndex(0, 3);
@@ -60,13 +97,15 @@ public class MineCombat extends PluginBase implements Listener{
 		Player player = event.getPlayer();
 		Item item = event.getItem();
 		
-		if(item.getId() == GUN_ITEM_ID){
-			if(containers.containsKey(player.getName())){
-				containers.get(player.getName()).startShoot();
-			}
-		}else{
-			if(containers.containsKey(player.getName())){
-				containers.get(player.getName()).stopShoot();
+		if(this.status == STATUS_ONGOING){
+			if(item.getId() == GUN_ITEM_ID){
+				if(containers.containsKey(player.getName())){
+					containers.get(player.getName()).startShoot();
+				}
+			}else{
+				if(containers.containsKey(player.getName())){
+					containers.get(player.getName()).stopShoot();
+				}
 			}
 		}
 	}
@@ -91,10 +130,52 @@ public class MineCombat extends PluginBase implements Listener{
 		}
 	}
 	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+	@SuppressWarnings("serial")
+	public boolean onCommand(final CommandSender sender, Command command, String label, String[] args){
 		switch(command.getName()){
 		case "spawnpos":
+			if(args.length < 2){
+				sender.sendMessage(TextFormat.RED + "Usage: " + command.getUsage());
+				return true;
+			}
+			if(!(sender instanceof Player)){
+				sender.sendMessage(TextFormat.RED + "Please use this command in-game.");
+				return true;
+			}
+			final Player player = (Player)sender;
 			
+			switch(args[0].toLowerCase()){
+			case "red":
+				if(!position.containsKey(args[1])){
+					position.put(args[1], new HashMap<String, Object[]>());
+				}
+				position.get(args[1]).put("red", new Object[]{
+						player.getX(), player.getY(), player.getZ(), player.getLevel().getFolderName()
+				});
+				sender.sendMessage(TextFormat.GREEN + "Position " + args[1] + " for red team was set.");
+				return true;
+			case "blue":
+				if(!position.containsKey(args[1])){
+					position.put(args[1], new HashMap<String, Object[]>());
+				}
+				position.get(args[1]).put("blue", new Object[]{
+						player.getX(), player.getY(), player.getZ(), player.getLevel().getFolderName()
+				});
+				sender.sendMessage(TextFormat.GREEN + "Position " + args[1] + " for blue team was set.");
+				return true;
+			case "create":
+				if(position.containsKey(args[1])){
+					Map<String, Map<String, Object[]>> spawns = this.getConfig().get("spawn-pos", new LinkedHashMap<String, Map<String, Object[]>>());
+					spawns.put(args[1], position.get(args[1]));
+					this.getConfig().set("spawn-pos", spawns);
+					// this.getConfig().setNested("spawn-pos." + args[1], position.get(args[1]));
+					this.getConfig().save();
+					sender.sendMessage(TextFormat.GREEN + "Position " + args[1] + "was set successfully if you did everything right.");
+				}else{
+					sender.sendMessage(TextFormat.RED + "Position " + args[1] + " does not exist!");
+				}
+				return true;
+			}
 			return true;
 		case "momap":
 			
@@ -104,11 +185,42 @@ public class MineCombat extends PluginBase implements Listener{
 	}
 	
 	public void startGame(){
+		this.status = STATUS_ONGOING;
 		
+		containers.clear();
+		Map<String, Player> online = this.getServer().getOnlinePlayers();
+		
+		int red = 0, blue = 0;
+		for(String username : online.keySet()){
+			Player player = online.get(username);
+			
+			int team = red > blue ? TEAM_BLUE : TEAM_RED;
+			containers.put(player.getName(), new PlayerContainer(player, new Pistol(this, player), team));
+		}
+		this.getServer().broadcastMessage(TextFormat.GREEN + "Game is started. Enjoy!");
+		
+		this.getServer().getScheduler().scheduleDelayedTask(new StopGameTask(this), this.getConfig().get("game-game", 300) * 20);
 	}
 	
 	public void stopGame(){
+		this.status = STATUS_ONGOING;
 		
+		containers.clear();
+		
+		this.getServer().broadcastMessage(TextFormat.YELLOW + "Game is finished.");
+		this.getServer().getScheduler().scheduleDelayedRepeatingTask(new TickTask(this), 10, 10);
+	}
+	
+	public int countPlayers(int team){
+		if(this.status == STATUS_ONGOING){
+			int ret = 0;
+			
+			for(String player : containers.keySet()){
+				if(containers.get(player).getTeam() == team) ++ret; 
+			}
+			return ret;
+		}
+		return 0;
 	}
 	
 	public void onTick(){
--- src/main/java/me/onebone/minecombat/data/PlayerContainer.java
+++ src/main/java/me/onebone/minecombat/data/PlayerContainer.java
@@ -1,17 +1,38 @@
 package me.onebone.minecombat.data;
 
+/*
+ * MineCombat: FP..S? for Nukkit
+ * Copyright (C) 2016 onebone <jyc00410@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 import me.onebone.minecombat.gun.BaseGun;
 import cn.nukkit.Player;
 
 public class PlayerContainer{
 	private Player player;
 	private BaseGun gun;
+	private int team;
 	
 	private boolean active = true;
 	
-	public PlayerContainer(Player player, BaseGun gun){
+	public PlayerContainer(Player player, BaseGun gun, int team){
 		this.player = player;
 		this.gun = gun;
+		this.team = team;
 	}
 	
 	public void quit(){
@@ -23,6 +44,10 @@ public class PlayerContainer{
 	
 	public BaseGun getGun(){
 		return this.gun;
+	}
+	
+	public int getTeam(){
+		return this.team;
 	}
 	
 	public void setActive(){
