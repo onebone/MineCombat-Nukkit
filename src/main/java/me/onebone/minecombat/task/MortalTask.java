2826716561baaab14f57731bb694c5dcd0893f25
onebone <jyc00410@gmail.com>
2016-01-06 14:05:20 +0900

    Oops, missing file

---
+++ src/main/java/me/onebone/minecombat/task/MortalTask.java
@@ -0,0 +1,38 @@
+package me.onebone.minecombat.task;
+
+/*
+ * MineCombat: FP..S? for Nukkit
+ * Copyright (C) 2016 onebone <jyc00410@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+import me.onebone.minecombat.MineCombat;
+
+import cn.nukkit.scheduler.PluginTask;
+
+public class MortalTask extends PluginTask<MineCombat>{
+	private String player;
+	
+	public MortalTask(MineCombat plugin, String player){
+		super(plugin);
+		
+		this.player = player;
+	}
+	
+	public void onRun(int currentTick){
+		this.getOwner().setMortal(player);
+	}
+}
